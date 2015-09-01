f797c78add3396307731418faf5d93c6a249930f
Gary R. Van Sickle <grvs@users.sourceforge.net>
2015-08-31 20:08:32 -0500

    Stubbed in TypeManager.

---
+++ src/TypeManager.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of UltimateCodeSearch.
+ *
+ * UltimateCodeSearch is free software: you can redistribute it and/or modify it under the
+ * terms of version 3 of the GNU General Public License as published by the Free
+ * Software Foundation.
+ *
+ * UltimateCodeSearch is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * UltimateCodeSearch.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file */
+
+#include "TypeManager.h"
+
+TypeManager::TypeManager()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+TypeManager::~TypeManager()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/TypeManager.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2015 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of UltimateCodeSearch.
+ *
+ * UltimateCodeSearch is free software: you can redistribute it and/or modify it under the
+ * terms of version 3 of the GNU General Public License as published by the Free
+ * Software Foundation.
+ *
+ * UltimateCodeSearch is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * UltimateCodeSearch.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file */
+
+#ifndef TYPEMANAGER_H_
+#define TYPEMANAGER_H_
+
+/*
+ *
+ */
+class TypeManager
+{
+public:
+	TypeManager();
+	virtual ~TypeManager();
+};
+
+#endif /* TYPEMANAGER_H_ */
