1bb29cec4008bf37bb48782ffa39d70cea0ec954
Gary R. Van Sickle <grvs@users.sourceforge.net>
2015-11-07 20:49:10 -0600

    Initial commit of DirInclusionManager.{cpp,h}.

---
+++ src/DirInclusionManager.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2015 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of UltimateCodeSearch.
+ *
+ * UltimateCodeSearch is free software: you can redistribute it and/or modify it under the
+ * terms of version 3 of the GNU General Public License as published by the Free
+ * Software Foundation.
+ *
+ * UltimateCodeSearch is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * UltimateCodeSearch.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file */
+
+#ifndef DIRINCLUSIONMANAGER_H_
+#define DIRINCLUSIONMANAGER_H_
+
+/*
+ *
+ */
+class DirInclusionManager
+{
+public:
+	DirInclusionManager();
+	virtual ~DirInclusionManager();
+};
+
+#endif /* DIRINCLUSIONMANAGER_H_ */
