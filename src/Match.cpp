651c5351dd4054fc2136cc296fb0da895c1d4cca
Gary R. Van Sickle <grvs@users.sourceforge.net>
2016-01-20 19:01:27 -0600

    Using reverse search to find beginning of line.

--- src/Match.cpp
+++ src/Match.cpp
@@ -24,9 +24,13 @@
 Match::Match(const char *start_of_array, size_t array_size, size_t match_start_offset, size_t match_end_offset)
 {
 	auto line_ending = "\n";
-	auto line_start = std::find_end(start_of_array, start_of_array+match_start_offset,
-			line_ending, line_ending+1);
-	if(line_start == start_of_array+match_start_offset)
+	// Find the start of the line.
+	std::reverse_iterator<const char*> rstart(start_of_array+match_start_offset);
+	std::reverse_iterator<const char*> rend(start_of_array);
+
+	auto line_start_rit = std::find(rstart, rend, line_ending[0]);
+	const char *line_start;
+	if(line_start_rit == rend)
 	{
 		// The line has no starting '\n', so it must be the first line.
 		line_start = start_of_array;
@@ -34,9 +38,11 @@ Match::Match(const char *start_of_array, size_t array_size, size_t match_start_o
 	else
 	{
 		// The line had a starting '\n', clip it off.
-		++line_start;
+		line_start = line_start_rit.base();
 	}
-	auto line_end = std::find(start_of_array+match_start_offset, start_of_array+array_size, '\n');
+
+	// Find the end of the matched line.
+	auto line_end = std::find(start_of_array+match_start_offset, start_of_array+array_size, line_ending[0]);
 	m_pre_match = std::string(line_start, start_of_array+match_start_offset);
 	m_match = std::string(start_of_array+match_start_offset, start_of_array+match_end_offset);
 	m_post_match = std::string(start_of_array+match_start_offset+(match_end_offset-match_start_offset), line_end);
