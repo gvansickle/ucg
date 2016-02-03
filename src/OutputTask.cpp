cced9b4f0fc156df97fbca0cc882238ccb10fd64
Gary R. Van Sickle <grvs@users.sourceforge.net>
2016-02-02 19:35:14 -0600

    New file: file_presentation.at.  Some commenting in OutputTask.

--- src/OutputTask.cpp
+++ src/OutputTask.cpp
@@ -25,11 +25,13 @@
 OutputTask::OutputTask(bool flag_color, bool flag_nocolor, sync_queue<MatchList> &input_queue)
 	: m_input_queue(input_queue)
 {
-	// Determine if the output is going to a terminal.  If so we'll use colors, group the matches under
+	// Determine if the output is going to a terminal.  If so we'll use color by default, group the matches under
 	// the filename, etc.
 	m_output_is_tty = isatty(fileno(stdout));
 
 	// Determine whether to enable color or not.
+	// Color is enabled if explicitly specified with --color or
+	// if outputting to a TTY and --nocolor is not specified.
 	if(flag_color || (!flag_nocolor && m_output_is_tty))
 	{
 		m_enable_color = true;
