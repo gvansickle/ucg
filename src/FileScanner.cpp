a3f697a97ef41d795e1c1c900f47989e166872b0
Gary R. Van Sickle <grvs@users.sourceforge.net>
2016-02-13 20:18:32 -0600

    FileScanner.cpp now compiles without warnings.

--- src/FileScanner.cpp
+++ src/FileScanner.cpp
@@ -206,7 +206,7 @@ void FileScanner::AssignToNextCore()
 #endif
 }
 
-void FileScanner::ScanFileCpp11(const std::regex& expression, const char *file_data, size_t file_size, MatchList& ml)
+void FileScanner::ScanFileCpp11(const std::regex& /*expression*/, const char */*file_data*/, size_t /*file_size*/, MatchList& /*ml*/)
 {
 #ifndef HAVE_LIBPCRE
 	// Scan the mmapped file for the regex.
@@ -249,9 +249,12 @@ void FileScanner::ScanFileLibPCRE(const char *file_data, size_t file_size, Match
 	long long line_no = 1;
 	long long prev_lineno = 0;
 	const char *prev_lineno_search_end = file_data;
+	// Up-cast file_size, which is a size_t (unsigned) to a ptrdiff_t (signed) which should be able to handle the
+	// same positive range, and not cause issues when compared with the ints of ovector[].
+	std::ptrdiff_t signed_file_size = file_size;
 
 	// Loop while the start_offset is less than the file_size.
-	while(ovector[1] < file_size)
+	while(ovector[1] < signed_file_size)
 	{
 		int options = 0;
 		int start_offset = ovector[1];
@@ -260,7 +263,7 @@ void FileScanner::ScanFileLibPCRE(const char *file_data, size_t file_size, Match
 		if (ovector[0] == ovector[1])
 		{
 			// Yes, are we at the end of the file?
-			if (ovector[0] == file_size)
+			if (ovector[0] == signed_file_size)
 			{
 				// Yes, we're done searching.
 				break;
@@ -318,7 +321,7 @@ void FileScanner::ScanFileLibPCRE(const char *file_data, size_t file_size, Match
 				else if(false /** @todo utf8 */)
 				{
 					// Increment a whole UTF8 character.
-					while(ovector[1] < file_size)
+					while(ovector[1] < signed_file_size)
 					{
 						if((file_data[ovector[1]] & 0xC0) != 0x80)
 						{
