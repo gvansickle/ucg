2d1a565f38d98a32eb5333f805d283a4d34556fd
Gary R. Van Sickle <grvs@users.sourceforge.net>
2016-05-27 11:36:09 -0500

    Added sub Logger class.

---
+++ src/Logger.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2016 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of UniversalCodeGrep.
+ *
+ * UniversalCodeGrep is free software: you can redistribute it and/or modify it under the
+ * terms of version 3 of the GNU General Public License as published by the Free
+ * Software Foundation.
+ *
+ * UniversalCodeGrep is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * UniversalCodeGrep.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SRC_LOGGER_H_
+#define SRC_LOGGER_H_
+
+class Logger {
+public:
+	Logger();
+	virtual ~Logger();
+};
+
+#endif /* SRC_LOGGER_H_ */
