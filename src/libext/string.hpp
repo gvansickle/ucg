23f660034f6b9dcc9397cb46348b7ce3a6201030
Gary R. Van Sickle <grvs@users.sourceforge.net>
2016-06-22 19:43:07 -0500

    Fixed std::to_string() backfill.

--- src/libext/string.hpp
+++ src/libext/string.hpp
@@ -134,20 +134,22 @@ static_assert(std::is_trivially_destructible<microstring>::value, "microstring i
 
 #if !defined(HAVE_DECL_STD__TO_STRING)
 
-// We have to backfill for broken C++11 std libs.  See e.g. https://gcc.gnu.org/bugzilla/show_bug.cgi?id=61580 (fixed on gcc trunk 2015-11-13),
-// https://sourceware.org/ml/cygwin/2015-01/msg00251.html.
+// We have to backfill std::to_string() for broken C++11 std libs.  See e.g. https://gcc.gnu.org/bugzilla/show_bug.cgi?id=61580
+// (fixed on gcc trunk 2015-11-13), https://sourceware.org/ml/cygwin/2015-01/msg00251.html.
 
 namespace std
 {
 
 template <typename T>
-std::string std::to_string(T value)
+string to_string(T value)
 {
-	static_assert(std::is_integral_type<T>::value, "Parameter passed to std::to_string() must be integral type.");
-	std::stringstream temp_ss;
+	static_assert(is_integral<T>::value, "Parameter passed to std::to_string() must be integral type.");
+	stringstream temp_ss;
 	temp_ss << value;
 	return temp_ss.str();
 }
+
+} // namespace std
 
 #endif
 
