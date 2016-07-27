057727b3c0662ce4787268ea7b6ea1d5090d746a
Gary R. Van Sickle <grvs@users.sourceforge.net>
2016-07-26 22:05:17 -0500

    Added lorem ipsum text.

--- tests/dummy-file-gen.cpp
+++ tests/dummy-file-gen.cpp
@@ -15,10 +15,65 @@
  * UniversalCodeGrep.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <iostream>
+
+extern const char *f_lorem_ipsum_text;
+
 int main(int argc, char **argv)
 {
+	std::cout << f_lorem_ipsum_text << std::endl;
 	return 0;
 }
 
 
-
+const char *f_lorem_ipsum_text =
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. At iam decimum annum in spelunca iacet. Parvi enim primo ortu sic iacent, tamquam omnino sine animo sint. Sed tempus est, si videtur, et recta quidem ad me. Cyrenaici quidem non recusant; Duo Reges: constructio interrete. Audeo dicere, inquit. Cum praesertim illa perdiscere ludus esset. Urgent tamen et nihil remittunt. Quo tandem modo?\n"
+		"\n"
+		"Sed tamen omne, quod de re bona dilucide dicitur, mihi praeclare dici videtur. Te enim iudicem aequum puto, modo quae dicat ille bene noris. Haec quo modo conveniant, non sane intellego.\n"
+		"\n"
+		"Negat esse eam, inquit, propter se expetendam. Nam ista vestra: Si gravis, brevis; An, partus ancillae sitne in fructu habendus, disseretur inter principes civitatis, P. Quod autem satis est, eo quicquid accessit, nimium est; Atqui perspicuum est hominem e corpore animoque constare, cum primae sint animi partes, secundae corporis. Aut haec tibi, Torquate, sunt vituperanda aut patrocinium voluptatis repudiandum. Neminem videbis ita laudatum, ut artifex callidus comparandarum voluptatum diceretur. Illud mihi a te nimium festinanter dictum videtur, sapientis omnis esse semper beatos; Dolor ergo, id est summum malum, metuetur semper, etiamsi non aderit; Quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret?\n"
+		"\n"
+		"Non potes, nisi retexueris illa. Certe, nisi voluptatem tanti aestimaretis. Facit enim ille duo seiuncta ultima bonorum, quae ut essent vera, coniungi debuerunt; Haec dicuntur inconstantissime. Cuius ad naturam apta ratio vera illa et summa lex a philosophis dicitur. Magna laus. Maximas vero virtutes iacere omnis necesse est voluptate dominante.\n"
+		"\n"
+		"Quod autem satis est, eo quicquid accessit, nimium est; Bona autem corporis huic sunt, quod posterius posui, similiora. At miser, si in flagitiosa et vitiosa vita afflueret voluptatibus. Videsne quam sit magna dissensio? De malis autem et bonis ab iis animalibus, quae nondum depravata sint, ait optime iudicari. Tum Piso: Quoniam igitur aliquid omnes, quid Lucius noster? Scaevola tribunus plebis ferret ad plebem vellentne de ea re quaeri. Ut alios omittam, hunc appello, quem ille unum secutus est. Atque ab his initiis profecti omnium virtutum et originem et progressionem persecuti sunt.\n"
+		"\n"
+		"Non autem hoc: igitur ne illud quidem. Ab his oratores, ab his imperatores ac rerum publicarum principes extiterunt.\n"
+		"\n"
+		"Sed ad rem redeamus; Quorum altera prosunt, nocent altera. Cur deinde Metrodori liberos commendas? In his igitur partibus duabus nihil erat, quod Zeno commutare gestiret. Omnium enim rerum principia parva sunt, sed suis progressionibus usa augentur nec sine causa; Neque solum ea communia, verum etiam paria esse dixerunt. Graece donan, Latine voluptatem vocant. Mihi quidem Antiochum, quem audis, satis belle videris attendere. Respondeat totidem verbis. Huic mori optimum esse propter desperationem sapientiae, illi propter spem vivere.\n"
+		"\n"
+		"Ab his oratores, ab his imperatores ac rerum publicarum principes extiterunt. Mihi, inquam, qui te id ipsum rogavi? Id est enim, de quo quaerimus. Si longus, levis; Aliter homines, aliter philosophos loqui putas oportere? Quae cum praeponunt, ut sit aliqua rerum selectio, naturam videntur sequi;\n"
+		"\n"
+		"Atqui haec patefactio quasi rerum opertarum, cum quid quidque sit aperitur, definitio est. Nihil opus est exemplis hoc facere longius. Quid enim est a Chrysippo praetermissum in Stoicis? Longum est enim ad omnia respondere, quae a te dicta sunt.\n"
+		"\n"
+		"Quae est igitur causa istarum angustiarum? Nunc vero a primo quidem mirabiliter occulta natura est nec perspici nec cognosci potest. Expressa vero in iis aetatibus, quae iam confirmatae sunt. Audeo dicere, inquit. In schola desinis. Potius ergo illa dicantur: turpe esse, viri non esse debilitari dolore, frangi, succumbere. Cum id fugiunt, re eadem defendunt, quae Peripatetici, verba. Eaedem enim utilitates poterunt eas labefactare atque pervertere.\n"
+		"\n"
+		"Non potes, nisi retexueris illa. Nunc vides, quid faciat. Nemo nostrum istius generis asotos iucunde putat vivere. Quos quidem tibi studiose et diligenter tractandos magnopere censeo. Poterat autem inpune; Si verbum sequimur, primum longius verbum praepositum quam bonum.\n"
+		"\n"
+		"Quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? Duae sunt enim res quoque, ne tu verba solum putes. Et quidem illud ipsum non nimium probo et tantum patior, philosophum loqui de cupiditatibus finiendis. Illa tamen simplicia, vestra versuta.\n"
+		"\n"
+		"Sin autem est in ea, quod quidam volunt, nihil impedit hanc nostram comprehensionem summi boni. Maximas vero virtutes iacere omnis necesse est voluptate dominante. Tum Lucius: Mihi vero ista valde probata sunt, quod item fratri puto. Quamquam id quidem, infinitum est in hac urbe; Non modo carum sibi quemque, verum etiam vehementer carum esse? Vitiosum est enim in dividendo partem in genere numerare. Egone non intellego, quid sit don Graece, Latine voluptas? Negat enim summo bono afferre incrementum diem. Hoc loco discipulos quaerere videtur, ut, qui asoti esse velint, philosophi ante fiant.\n"
+		"\n"
+		"Stoicos roga. Nos commodius agimus. Laboro autem non sine causa; Quae diligentissime contra Aristonem dicuntur a Chryippo. Quid est igitur, inquit, quod requiras? Quid, si etiam iucunda memoria est praeteritorum malorum? In qua quid est boni praeter summam voluptatem, et eam sempiternam? Ergo opifex plus sibi proponet ad formarum quam civis excellens ad factorum pulchritudinem?\n"
+		"\n"
+		"At enim, qua in vita est aliquid mali, ea beata esse non potest. Cave putes quicquam esse verius. Bonum patria: miserum exilium. Ad eos igitur converte te, quaeso. Consequens enim est et post oritur, ut dixi. Quae diligentissime contra Aristonem dicuntur a Chryippo.\n"
+		"\n"
+		"Facillimum id quidem est, inquam. Illud non continuo, ut aeque incontentae. Ita relinquet duas, de quibus etiam atque etiam consideret.\n"
+		"\n"
+		"Quid ergo attinet gloriose loqui, nisi constanter loquare? Eiuro, inquit adridens, iniquum, hac quidem de re; Ut id aliis narrare gestiant? Nullus est igitur cuiusquam dies natalis. Falli igitur possumus. Unum nescio, quo modo possit, si luxuriosus sit, finitas cupiditates habere.\n"
+		"\n"
+		"Tamen a proposito, inquam, aberramus. Quid turpius quam sapientis vitam ex insipientium sermone pendere? Hinc ceteri particulas arripere conati suam quisque videro voluit afferre sententiam.\n"
+		"\n"
+		"Haec quo modo conveniant, non sane intellego. Non est ista, inquam, Piso, magna dissensio. Ergo illi intellegunt quid Epicurus dicat, ego non intellego? Conferam tecum, quam cuique verso rem subicias; Graecum enim hunc versum nostis omnes-: Suavis laborum est praeteritorum memoria. Itaque contra est, ac dicitis; Suo genere perveniant ad extremum;\n"
+		"\n"
+		"Beatus sibi videtur esse moriens. Estne, quaeso, inquam, sitienti in bibendo voluptas? Hoc non est positum in nostra actione. Atque haec coniunctio confusioque virtutum tamen a philosophis ratione quadam distinguitur. Restatis igitur vos; An hoc usque quaque, aliter in vita? Sequitur disserendi ratio cognitioque naturae; Quodsi ipsam honestatem undique pertectam atque absolutam. A quibus propter discendi cupiditatem videmus ultimas terras esse peragratas. Unum est sine dolore esse, alterum cum voluptate.\n"
+		"\n"
+		"Cum ageremus, inquit, vitae beatum et eundem supremum diem, scribebamus haec. Quia nec honesto quic quam honestius nec turpi turpius. Haec et tu ita posuisti, et verba vestra sunt. Sic enim censent, oportunitatis esse beate vivere.\n"
+		"\n"
+		"Vestri haec verecundius, illi fortasse constantius. Consequentia exquirere, quoad sit id, quod volumus, effectum. Igitur neque stultorum quisquam beatus neque sapientium non beatus. At certe gravius. Ergo in gubernando nihil, in officio plurimum interest, quo in genere peccetur. Ab hoc autem quaedam non melius quam veteres, quaedam omnino relicta. Atqui haec patefactio quasi rerum opertarum, cum quid quidque sit aperitur, definitio est.\n"
+		"\n"
+		"Ad corpus diceres pertinere-, sed ea, quae dixi, ad corpusne refers? Ita relinquet duas, de quibus etiam atque etiam consideret. Quamquam tu hanc copiosiorem etiam soles dicere. Nam Pyrrho, Aristo, Erillus iam diu abiecti. Bestiarum vero nullum iudicium puto. Et harum quidem rerum facilis est et expedita distinctio. Itaque hic ipse iam pridem est reiectus;\n"
+		"\n"
+		"Verum hoc idem saepe faciamus. Non minor, inquit, voluptas percipitur ex vilissimis rebus quam ex pretiosissimis. Num igitur eum postea censes anxio animo aut sollicito fuisse? Nunc haec primum fortasse audientis servire debemus.\n"
+		"\n"
+		"Sic, et quidem diligentius saepiusque ista loquemur inter nos agemusque communiter. Bonum incolumis acies: misera caecitas. Semper enim ex eo, quod maximas partes continet latissimeque funditur, tota res appellatur. Haec para/doca illi, nos admirabilia dicamus. Quis suae urbis conservatorem Codrum, quis Erechthei filias non maxime laudat?\n"
+;
