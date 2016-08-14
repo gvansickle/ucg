4db1bb603ecca98d29b6bd87867c0e7165a59982
Gary R. Van Sickle <grvs@users.sourceforge.net>
2016-08-14 16:22:08 -0500

    New subdir/lib: future.

---
+++ src/future/memory.hpp
@@ -0,0 +1,15 @@
+/*
+ * memory.hpp
+ *
+ *  Created on: Aug 14, 2016
+ *      Author: gary
+ */
+
+#ifndef SRC_FUTURE_MEMORY_HPP_
+#define SRC_FUTURE_MEMORY_HPP_
+
+
+
+
+
+#endif /* SRC_FUTURE_MEMORY_HPP_ */
