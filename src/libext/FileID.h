92c32d8f77899680cb3a52bfcfc18451e3522fc5
Gary R. Van Sickle <grvs@users.sourceforge.net>
2016-11-13 10:45:18 -0600

    Split FileDescriptor class into its own header.

--- src/libext/FileID.h
+++ src/libext/FileID.h
@@ -34,6 +34,7 @@
 
 #include "integer.hpp"
 #include "filesystem.hpp"
+#include "FileDescriptor.hpp"
 
 
 // Forward declarations.
@@ -50,6 +51,22 @@ enum FileType
 	FT_SYMLINK,
 	FT_STAT_FAILED
 };
+/// Stream insertion operator for FileType enum.
+inline std::ostream& operator<<(std::ostream& out, const FileType value){
+    const char* s = 0;
+#define M_ENUM_CASE(p) case(p): s = #p; break;
+    switch(value){
+        M_ENUM_CASE(FT_UNINITIALIZED);
+        M_ENUM_CASE(FT_UNKNOWN);
+        M_ENUM_CASE(FT_REG);
+        M_ENUM_CASE(FT_DIR);
+		M_ENUM_CASE(FT_SYMLINK);
+		M_ENUM_CASE(FT_STAT_FAILED);
+    }
+#undef M_ENUM_CASE
+
+    return out << s;
+}
 
 /**
  * Only one may be specified.
