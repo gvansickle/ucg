dd691e8fe45bec716e7492ef25ad0d10b0a52d9c
Gary R. Van Sickle <grvs@users.sourceforge.net>
2016-12-25 21:55:19 -0600

    Checking if __cpp_lib_make_unique is defined by clang.

--- src/future/memory.hpp
+++ src/future/memory.hpp
@@ -26,11 +26,11 @@
 
 namespace std
 {
-#if !defined(__clang__) ///@todo For some reason I can't seem to figure out, The #if logic below
+//#if !defined(__clang__) ///@todo For some reason I can't seem to figure out, The #if logic below
                         /// does not exclude this definition on clang.  Clang doesn't define __cpp_lib_make_unique,
                         /// and even though configure correctly detects HAVE_DECL_STD__MAKE_UNIQUE_INT_ == 1, my definition
                         /// still gets included, then conflicts with the definition clang does in fact have.
-#if !defined(__cpp_lib_make_unique) && (HAVE_DECL_STD__MAKE_UNIQUE_INT_ == 0)  // C++14 feature.
+#if !defined(__cpp_lib_make_unique)// && (HAVE_DECL_STD__MAKE_UNIQUE_INT_ == 0)  // C++14 feature.
 /// Define our own make_unique<>() substitute.
 /// @note SFINAE here to fail this for array types.
 /// @note No need to check for __cpp_variadic_templates, it's C++11 and introduced in gcc 4.3.
@@ -41,7 +41,7 @@ std::unique_ptr<T> make_unique(Args&&... args)
 	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 #endif
-#endif
+//#endif
 }
 
 #endif /* SRC_FUTURE_MEMORY_HPP_ */
