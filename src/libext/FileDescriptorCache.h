fbe1d2bd649e858d9165e820f225dc6b9750f26a
Gary R. Van Sickle <grvs@users.sourceforge.net>
2016-12-28 22:38:32 -0600

    New files.

---
+++ src/libext/FileDescriptorCache.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of UniversalCodeGrep.
+ *
+ * UniversalCodeGrep is free software: you can redistribute it and/or modify it under the
+ * terms of version 3 of the GNU General Public License as published by the Free
+ * Software Foundation.
+ *
+ * UniversalCodeGrep is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * UniversalCodeGrep.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file */
+
+#ifndef SRC_LIBEXT_FILEDESCRIPTORCACHE_H_
+#define SRC_LIBEXT_FILEDESCRIPTORCACHE_H_
+
+#include <config.h>
+
+#include "FileDescriptor.hpp"
+#include "filesystem.hpp"
+
+/*
+ *
+ */
+class FileDescriptorCache
+{
+public:
+	FileDescriptorCache();
+	~FileDescriptorCache();
+
+private:
+
+
+};
+
+#endif /* SRC_LIBEXT_FILEDESCRIPTORCACHE_H_ */
