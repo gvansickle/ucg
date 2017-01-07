05ec833c0e13afd6644d6f6ad27c39767bb9c6ef
Gary R. Van Sickle <grvs@users.sourceforge.net>
2017-01-06 21:36:50 -0600

    Put SSE4.2 literal match logic back.

--- src/FileScanner_sse4_2.cpp
+++ src/FileScanner_sse4_2.cpp
@@ -303,7 +303,7 @@ int FileScanner::LiteralMatch_sse4_2(const char *file_data, size_t file_size, si
 	const char* str_match;
 	const size_t bytes_to_search = file_size - start_offset;
 
-	if(0)//m_literal_search_string_len <= 16)
+	if(m_literal_search_string_len <= 16)
 	{
 		str_match = (const char*)MV_USE(memmem_short_pattern, ISA_x86_64::SSE4_2)((const void*)(file_data+start_offset), bytes_to_search,
 				(const void *)m_literal_search_string.get(), m_literal_search_string_len);
